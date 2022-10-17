473165aa93832140a41f7ada02b76a1c4180e0dd
Allen Guan <me@nella17.tw>
2022-10-17 19:46:11 +0800

    [Lab5] fix indent

--- Lab-5-Character-LCD-Control/lab5.v
+++ Lab-5-Character-LCD-Control/lab5.v
@@ -1,14 +1,14 @@
 `timescale 1ns / 1ps
 /////////////////////////////////////////////////////////
 module lab5(
-  input clk,
-  input reset_n,
-  input [3:0] usr_btn,
-  output [3:0] usr_led,
-  output LCD_RS,
-  output LCD_RW,
-  output LCD_E,
-  output [3:0] LCD_D
+    input clk,
+    input reset_n,
+    input [3:0] usr_btn,
+    output [3:0] usr_led,
+    output LCD_RS,
+    output LCD_RW,
+    output LCD_E,
+    output [3:0] LCD_D
 );
 
     // turn off all the LEDs
@@ -20,40 +20,40 @@ module lab5(
     reg [127:0] row_B = "show a message.."; // Initialize the text of the second row.
 
     LCD_module lcd0(
-      .clk(clk),
-      .reset(~reset_n),
-      .row_A(row_A),
-      .row_B(row_B),
-      .LCD_E(LCD_E),
-      .LCD_RS(LCD_RS),
-      .LCD_RW(LCD_RW),
-      .LCD_D(LCD_D)
+        .clk(clk),
+        .reset(~reset_n),
+        .row_A(row_A),
+        .row_B(row_B),
+        .LCD_E(LCD_E),
+        .LCD_RS(LCD_RS),
+        .LCD_RW(LCD_RW),
+        .LCD_D(LCD_D)
     );
-        
+
     debounce btn_db0(
-      .clk(clk),
-      .btn_input(usr_btn[3]),
-      .btn_output(btn_level)
+        .clk(clk),
+        .btn_input(usr_btn[3]),
+        .btn_output(btn_level)
     );
-        
+
     always @(posedge clk) begin
-      if (~reset_n)
-        prev_btn_level <= 1;
-      else
-        prev_btn_level <= btn_level;
+        if (~reset_n)
+            prev_btn_level <= 1;
+        else
+            prev_btn_level <= btn_level;
     end
 
     assign btn_pressed = (btn_level == 1 && prev_btn_level == 0);
 
     always @(posedge clk) begin
-      if (~reset_n) begin
-        // Initialize the text when the user hit the reset button
-        row_A = "Press BTN3 to   ";
-        row_B = "show a message..";
-      end else if (btn_pressed) begin
-        row_A <= "Hello, World!   ";
-        row_B <= "Demo of the LCD.";
-      end
+        if (~reset_n) begin
+            // Initialize the text when the user hit the reset button
+            row_A = "Press BTN3 to   ";
+            row_B = "show a message..";
+        end else if (btn_pressed) begin
+            row_A <= "Hello, World!   ";
+            row_B <= "Demo of the LCD.";
+        end
     end
 
 endmodule
